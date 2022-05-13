359168997eb8d385b1ca3746046e84631c9fc250
Murtaza Pakawala <mypaka52@gmail.com>
2022-05-13 10:01:47 +1000

    using middle ware to add new contacts on our home page

--- contact_list/index.js
+++ contact_list/index.js
@@ -5,9 +5,25 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded());
+
+var contacts = [
+  {
+    name: "tony",
+    phone: "098359287",
+  },
+];
 app.get("/", function (req, res) {
-  return res.render("index");
+  return res.render("index", {
+    contact_list: contacts,
+  });
 });
+app.post("/create-contact", function (req, res) {
+  contacts.push(req.body);
+  console.log(req.body);
+  return res.redirect("back");
+});
+
 app.listen(port, function (err) {
   if (err) {
     return;
